964537d28ce056d2a8526f728e7af286057a6b6e
Mamedes <mamedeser@gmail.com>
2020-08-04 08:27:29 -0400

    first commit

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+const TeacherItem: React.FC = () => {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars0.githubusercontent.com/u/25774384?s=60&v=4"
+          alt="MamedesEron"
+        />
+        <div>
+          <strong>MamedesEron</strong>
+          <span>Enginer</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br />
+        <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/Hora
+          <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default TeacherItem;
